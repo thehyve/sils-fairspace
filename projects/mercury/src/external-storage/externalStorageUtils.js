d691961447c01374c6fb062feaf0ebf139e77eed
ewelinagr <ewelina@thehyve.nl>
2022-04-06 15:43:50 +0200

    Fix linter setup.

--- projects/mercury/src/external-storage/externalStorageUtils.js
+++ projects/mercury/src/external-storage/externalStorageUtils.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 import {encodePath, joinPathsAvoidEmpty} from "../file/fileUtils";
 import * as consts from "../constants";
 import {PATH_SEPARATOR} from "../constants";
